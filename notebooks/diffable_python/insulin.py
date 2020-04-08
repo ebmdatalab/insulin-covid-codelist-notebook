c44c0f437f002b5c136195cd9aa1bcfe577a8bf2
Brian <brianmkcebm@gmail.com>
2020-04-08 21:17:07 +0100

    first commit insulin

---
+++ notebooks/diffable_python/insulin.py
@@ -0,0 +1,47 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: all
+#     notebook_metadata_filter: all,-language_info
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.3.3
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# The following notebook contains Snomed/NHS Dictionary of Medicines and Devices code for insulins from the [BNF code section insulins](https://openprescribing.net/bnf/060101/).
+
+from ebmdatalab import bq
+import os
+import pandas as pd
+
+# +
+sql = '''WITH bnf_codes AS (
+  SELECT bnf_code FROM hscic.presentation WHERE 
+bnf_code LIKE '0601012%'  #BNF section insulins
+)
+
+SELECT "vmp" AS type, id, bnf_code, nm
+FROM dmd.vmp
+WHERE bnf_code IN (SELECT * FROM bnf_codes)
+
+UNION ALL
+
+SELECT "amp" AS type, id, bnf_code, descr
+FROM dmd.amp
+WHERE bnf_code IN (SELECT * FROM bnf_codes)
+
+ORDER BY type, bnf_code, id'''
+
+arb_codelist = bq.cached_read(sql, csv_path=os.path.join('..','data','arb_codelist.csv'))
+pd.set_option('display.max_rows', None)
+pd.set_option('display.width', None)
+arb_codelist
+# -
+
+
